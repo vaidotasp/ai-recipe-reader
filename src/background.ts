1941a02f6dca71db972a4fdea95210e3f300c7bb
Vaidotas Piekus <vaidotasp@gmail.com>
2025-02-23 10:28:04 -0500

    clean out service worker

--- src/background.ts
+++ src/background.ts
@@ -1,38 +1,7 @@
-// import { extractDOMContent } from "./work";
-
 // snap extension to the side panel
 chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch((error) => console.error(error));
-
-chrome.runtime.onInstalled.addListener((event) => {
-  console.log("DEBUG: runtime.oninstalled.event");
-  console.log("runtime installed");
-  console.log(event);
-
-  chrome.action.setBadgeText({
-    text: "HELLO",
-  });
-});
-
-// chrome.runtime.onMessage.addListener(async (message, _sender, sendResponse) => {
-//   //
-//   if (message === "init-scan") {
-//     console.log("init scan received from react into service worker");
-//     const result = await chrome.scripting.executeScript({
-//       target: { tabId: activeInfo.tabId },
-//       files: ["parse-and-extract-content.js"],
-//     });
-//     sendResponse(result);
-//   }
-// });
-
-chrome.tabs.onActivated.addListener(async (activeInfo) => {
-  // const result = await chrome.scripting.executeScript({
-  //   target: { tabId: activeInfo.tabId },
-  //   files: ["parse-and-extract-content.js"],
-  // });
-});
 
 chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
   await chrome.sidePanel.setOptions({
@@ -40,9 +9,4 @@ chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
     path: "index.html",
     enabled: true,
   });
-
-  // const result = await chrome.scripting.executeScript({
-  //   target: { tabId },
-  //   files: ["parse-and-extract-content.js"],
-  // });
 });
